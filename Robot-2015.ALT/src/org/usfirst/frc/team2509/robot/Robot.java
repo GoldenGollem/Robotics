2f8a0622fd4ad262f576ee8f58406dd021a5b441
Nathanial <goldengollem@gmail.com>
2016-12-12 12:58:50 -0600

    Cleaned Code

--- Robot-2015.ALT/src/org/usfirst/frc/team2509/robot/Robot.java
+++ Robot-2015.ALT/src/org/usfirst/frc/team2509/robot/Robot.java
@@ -1,6 +1,4 @@
-
 package org.usfirst.frc.team2509.robot;
-
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -14,13 +12,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 	SpeedController robotDrivelMotor;
     SpeedController robotDriverMotor;
@@ -43,12 +34,7 @@ public class Robot extends IterativeRobot {
     SendableChooser chooser;
     Joystick leftStick;
     Joystick rightStick;
-    
-	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+
     public void robotInit() {
     	RoboStart();
     	comp.start();
@@ -57,16 +43,7 @@ public class Robot extends IterativeRobot {
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
     }
-    
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
     public void autonomousInit() {
     	
     	autoSelected = (String) chooser.getSelected();
@@ -74,9 +51,6 @@ public class Robot extends IterativeRobot {
 		System.out.println("Auto selected: " + autoSelected);
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
     public void autonomousPeriodic() {
     	switch(autoSelected) {
     	case customAuto:
@@ -89,17 +63,13 @@ public class Robot extends IterativeRobot {
     	}
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
     	gripPiston.set(DoubleSolenoid.Value.kReverse);
     	guidePiston.set(DoubleSolenoid.Value.kReverse);
     	armPiston.set(DoubleSolenoid.Value.kForward);
         while(isEnabled()&&isOperatorControl()){
         	robotDrive.tankDrive(leftStick.getRawAxis(1), rightStick.getRawAxis(1));
-        	
-        	
+        	OpControl();
         }
     }
     
@@ -145,7 +115,7 @@ public class Robot extends IterativeRobot {
         leftBottom  = new DigitalInput(3);
         rightBottom = new DigitalInput(1);
     }
-    public void OpControll(){
+    public void OpControl(){
     	while(isEnabled()&&isOperatorControl()){
     		while(leftStick.getRawButton(2)){
         		if(leftTop.get()==false&&rightTop.get()==false){
